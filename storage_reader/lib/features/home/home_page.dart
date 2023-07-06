fc2d761ea2bd9c9dc1abf0ebb30ff7eac78e1798
Adnan <adnankuza@gmail.com>
2023-07-07 02:13:28 +0300

    added qr page

--- storage_reader/lib/features/home/home_page.dart
+++ storage_reader/lib/features/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:storage_reader/app/constants/colors.dart';
 import 'package:storage_reader/app/widgets/button.dart';
 import 'package:storage_reader/app/widgets/title_appbar.dart';
+import 'package:storage_reader/features/qrcode/presentation/qrcode_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -46,7 +47,7 @@ class HomePage extends StatelessWidget {
                 const SizedBox(height: 60),
                 Center(
                   child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.6,
+                      width: MediaQuery.sizeOf(context).width * 0.6,
                       child: Image.asset('assets/images/qr_background.png')),
                 ),
                 const SizedBox(height: 70),
@@ -54,7 +55,9 @@ class HomePage extends StatelessWidget {
                   child: SizedBox(
                       width: 160,
                       child: CustomElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>QRViewPage()));
+                        },
                         title: "Scan",
                         borderRadius: 22,
                         buttonColor: AppColors.mainblue,
